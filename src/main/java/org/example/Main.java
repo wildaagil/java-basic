b21c934006d46ad19025950fc35ecdcdc539ca14
wildaagil <wildaagilni@gmail.com>
2023-08-10 22:03:46 +0700

    type data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,19 @@ public class Main {
         comments */
         System.out.println("Hello world!"); //print char
         System.out.println(5); //print int
+
+        //this is variable
+        String name = "Agil";
+        System.out.println("Hai" + name);
+
+        int a = 1, b = 2;
+        int c = a + b;
+        System.out.println(c);
+
+        float numFloat = 3.83F;
+        char inName = 'A';
+        boolean theBool = true;
+
+        System.out.println("Initials" + " " + inName + " " + "IPK" + " " + numFloat + " " + "is a" +" " + theBool );
     }
 }

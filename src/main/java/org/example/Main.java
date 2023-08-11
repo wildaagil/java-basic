5083e07370b53d548cac0ec14eaab6f4ff877660
wildaagil <wildaagilni@gmail.com>
2023-08-11 23:02:32 +0700

    operator with input number

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /* The code below is an example of
@@ -20,5 +22,18 @@ public class Main {
         boolean theBool = true;
 
         System.out.println("Initials" + " " + inName + " " + "IPK" + " " + numFloat + " " + "is a" +" " + theBool );
+
+        //operator example with input
+        int num1, num2, hasil;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("masukan angka: ");
+        num1 = input.nextInt();
+        System.out.print("masukan angka: ");
+        num2 = input.nextInt();
+
+        hasil = num1 + num2;
+        System.out.println("maka hasilnya: " + hasil);
     }
 }
